7223ad3fd85b21a8d0f145dd3cd2e5f3a0b19ceb
Romain Thomine <romain.thomine007@gmail.com>
2021-02-02 16:57:24 +0100

    02/02/2021

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -5,27 +5,97 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use App\Models\Property;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PropertyController extends BaseController
 {
     public function getById($id){
-        $property = Property::findOrFail($id);
-        return $Properties;
+        $property = Property::find($id);
+        if($property == NULL){
+            return response()->json(['message' => 'Propriété non trouvé'], 404);
+        }
+        return response()->json($property);
     }
 
     public function getAll(){
-      return Property::all();
+        $property = Property::all();
+        return response()->json($property);
     }
     public function create(Request $request){
+        $validator = Validator::make($request->all(),
+        [
+           'price'=>'required|numeric|min:4|max:10',
+           'label'=>'required|max:50',
+           'descriptive'=>'required|max:255',
+           'longitude'=>'required',
+           'latitude'=>'required',
+           'city'=>'required|max:40',
+           'address'=>'required|max:80',
+           'zipcode'=>'required|max:8',
+           'livingArea'=>'required|max:5',
+           'area'=>'required|max:5',
+           'gardenArea'=>'required|max:5',
+           'floorNumber'=>'required|max:5',
+           'piecesNumber'=>'required|max:5',
+           'bedroomNumber'=>'required|max:5',
+           'bathroomNumber'=>'required|max:5',
+           'wcNumber'=>'required|max:5',
+           'buildingNumber'=>'nullable|max:5',
+           'bearing'=>'nullable|max:5',
+           'doorNumber'=>'nullable|max:5',
+           'garden'=>'boolean',
+           'garage'=>'boolean',
+           'cellar'=>'boolean',
+           'atic'=>'boolean',
+           'parking'=>'boolean',
+           'opticalFibre'=>'boolean',
+           'swimmingPool'=>'boolean',
+           'balcony'=>'boolean',
+           'interestedClientId'=>'nullable',
+           'archive'=>'boolean'
+
+        ])->validate();
         $property = Property::create($request->all());
-        return $property;
+        return response()->json($property);
     }
     public function delete($id){
         $property = Property::destroy($id);
     }
     public function update(Resquest $request){
-       $property = Property::find($request->route('id'));
-       $property->label = $request->label;
-       $property->save();
+       $validator = Validator::make($request->all(),
+       [
+           'price'=>'required|numeric|min:4|max:10',
+           'label'=>'required|max:50',
+           'descriptive'=>'required|max:255',
+           'longitude'=>'required',
+           'latitude'=>'required',
+           'city'=>'required|max:40',
+           'address'=>'required|max:80',
+           'zipcode'=>'required|max:8',
+           'livingArea'=>'required|max:5',
+           'area'=>'required|max:5',
+           'gardenArea'=>'required|max:5',
+           'floorNumber'=>'required|max:5',
+           'piecesNumber'=>'required|max:5',
+           'bedroomNumber'=>'required|max:5',
+           'bathroomNumber'=>'required|max:5',
+           'wcNumber'=>'required|max:5',
+           'buildingNumber'=>'nullable|max:5',
+           'bearing'=>'nullable|max:5',
+           'doorNumber'=>'nullable|max:5',
+           'garden'=>'boolean',
+           'garage'=>'boolean',
+           'cellar'=>'boolean',
+           'atic'=>'boolean',
+           'parking'=>'boolean',
+           'opticalFibre'=>'boolean',
+           'swimmingPool'=>'boolean',
+           'balcony'=>'boolean',
+           'interestedClientId'=>'nullable',
+           'archive'=>'boolean'
+    ])->validate();
+        $property = Property::find($request->route('id'));
+        $property->label = $request->label;
+        $property->save();
     }
 }
