b711d1f9f54204ae49185efd8378684cc4adc97a
Hugo Vanier De Saint Aunay <hugo@info80.fr>
2021-03-01 02:39:02 +0100

    création de toutes les méthodes liées aux rdv

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
+use App\Models\Client;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AppointmentController extends Controller
 {
@@ -17,46 +20,125 @@ class AppointmentController extends Controller
         //
     }
     public function getById($id)
-    {
-        $appointment = Appointment::findOrFail($id);
-        return response()->json($appointment);
+    {   
+        try {
+            $appointment = Appointment::findOrFail($id);
+            return response()->json($appointment);
+        } catch (\Exception $e) {
+            return response()->json('Rendez-vous non trouvé', 404);
+        }
     }
     //
-    public function getByUser(Request $request)
+    public function getByUser($userId, Request $request)
     {
-        $appointments = Appointment::where('id_users', $request->route('id_users'))
-        ->whereDate('date', '>', date($request->get('dateStart')))
-        ->whereDate('date', '<', date($request->get('dateEnd')))
-        ->get();
-        return response()->json($appointment);
+        try {
+            $user = User::findOrFail($userId);
+            $appointments = Appointment::where('id_users', $user->id)
+                                            ->whereDate('date', '>', date($request->get('dateStart')))
+                                            ->whereDate('date', '<', date($request->get('dateEnd')))
+                                            ->get();
+            return response()->json($appointments);
+        } catch (\Exception $e) {
+            return response()->json('Utilisateur non trouvé', 404);
+        }
     }
     //
-    public function getByClient(Request $request)
+    public function getByClient($clientId, Request $request)
     {
-        $appointment = Appointment::where('id_clients', $request->route('id_clients'))
-        ->get();
-        return response()->json($appointment);
+        try {
+            $client = Client::findOrFail($clientId);
+            $appointments = Appointment::where('id_clients', $client->id)->get();
+            return response()->json($appointments);
+        } catch (\Exception $e) {
+            return response()->json('Client non trouvé', 404);
+        }
     }
-    //
+
     public function createAppointment(Request $request)
     {
-        $appointment = Appointment::where('id_users',$request->route('id_clients'));
-        $appointment->create();
-        return response()->json($appointment);
-   
+        $validator = Validator::make($request->all(), [
+            'city' => 'required|max:100',
+            'adress' => 'required|max:255',
+            'date' => 'required|date',
+            'appointment_type_id' => 'required|integer',
+            'user_id' => 'required|integer',
+            'client_id' => 'required|integer'
+        ],
+        [
+            'required' => 'Vous devez remplir ce champ',
+            'max' => 'Les données entrées sont trop longue, veuillez raccourcir',
+            'date' => 'A tester, sert à vérifier que c\'est une date'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $appointment = new Appointment();
+
+        $appointment->city = $request->city;
+        $appointment->adress = $request->adress;
+        $appointment->description = $request->description;
+        $appointment->date = $request->date;
+        $appointment->appointment_type_id = $request->appointment_type_id;
+        $appointment->user_id = $request->user_id;
+        $appointment->client_id = $request->client_id;
+
+        $isSaved = $appointment->save();
+
+        if($isSaved == true){
+            return response()->json(['message' => 'Le rendez-vous à bien été ajouté.']);
+        }else {
+            return response()->json(['message' => 'Il y a eu un problème lors de l\'ajout du rendez-vous.']);
+        }
     }
     //
-    public function updateAppointment($id){
-        $appointment = Appointment::find($id);
-        $appointment->date = input('date');
-        $appointment->save();
-        return response()->json($appointment);
+    public function updateAppointment($id, Request $request){
+        try {
+            $appointment = Appointment::findOrFail($id);
+            $validator = Validator::make($request->all(), [
+                'city' => 'required|max:100',
+                'adress' => 'required|max:255',
+                'date' => 'required|date',
+                'appointment_type_id' => 'required|integer',
+                'user_id' => 'required|integer',
+                'client_id' => 'required|integer'
+            ],
+            [
+                'required' => 'Vous devez remplir ce champ',
+                'max' => 'Les données entrées sont trop longue, veuillez raccourcir',
+                'date' => 'A tester, sert à vérifier que c\'est une date'
+            ]);
+            if ($validator->fails()) {
+                return response()->json($validator->errors());
+            }
+
+            $appointment->city = $request->city;
+            $appointment->adress = $request->adress;
+            $appointment->description = $request->description;
+            $appointment->date = $request->date;
+            $appointment->appointment_type_id = $request->appointment_type_id;
+            $appointment->user_id = $request->user_id;
+            $appointment->client_id = $request->client_id;
+    
+            $isSaved = $appointment->save();
+    
+            if($isSaved == true){
+                return response()->json(['message' => 'Le rendez-vous à bien été modifié.']);
+            }else {
+                return response()->json(['message' => 'Il y a eu un problème lors de la modification du rendez-vous.']);
+            }
+        } catch (\Exception $e) {
+            return response()->json('Rendez-vous non trouvé', 404);
+        }
     }      
     //
     public function deleteAppointment($id){
-        $appointment = Appointment::find($id);
-        $appointment->delete();
-        return response()->json('Rendez-vous archivé correctement.');
+        try {
+            $appointment = Appointment::findOrFail($id);
+            $appointment->delete();
+            return response()->json('Rendez-vous supprimer');
+        } catch (\Exception $e) {
+            return response()->json('Rendez-vous non trouvé', 404);
+        }
     }
 }
 
