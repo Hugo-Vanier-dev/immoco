b3aefc5513019a92dacf2e04df22c3d05cddf435
Hugo <hugovanierdesaintaunay@gmail.com>
2021-09-30 15:20:03 +0200

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        $this->middleware('api:auth', ['except' => ['login']]);
+        $this->middleware('auth', ['except' => ['login']]);
     }
 
           /**
