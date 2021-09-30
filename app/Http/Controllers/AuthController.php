f7a9875567aeaa92aca252434dcb204d54a4470c
Hugo <hugovanierdesaintaunay@gmail.com>
2021-09-30 15:34:25 +0200

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
 
 
         $credentials = $request->only(['mail', 'password']);
-        if (!$token = Auth::setTTL(2)->attempt($credentials)) {
+        if (!$token = Auth::setTTL(60)->attempt($credentials)) {
             return response()->json(['message' => 'Unauthorized'], 401);
         } 
 
